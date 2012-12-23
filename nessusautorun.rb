62c93501108b638fc9dd03f1202504291305cc75
Rory McCune <rory.mccune@scotsts.com>
2012-12-23 10:04:13 +0000

    Updated nessusautorun with URI parsing

--- nessusautorun.rb
+++ nessusautorun.rb
@@ -13,14 +13,32 @@ class NessusautoRun
       exit
     end
 
+    require 'uri'
+
 
     #ToDo: fix this up to check for the presence of a port and have it add it if needed
     #At the moment it's a hack for forgetful people (e.g. me)
-    unless host =~ /8834\/*$/
-      host = host + ':8834'
+    #unless host =~ /8834\/*$/
+    #  host = host + ':8834'
+    #end
+    
+    host_url = URI.parse(host)
+
+    unless host_url.class == URI::HTTPS
+      abort("Sure you got the hostname right?")
     end
 
-    @host = host
+    #Assumption here is that Nessus runs on 8834 in future this should be an option but at the moment we'll hardcode it
+    unless host_url.port == 8834
+      host_url.set_port = 8834
+    end
+
+
+
+
+
+
+    @host = host_url.to_s
 
     @agent = Mechanize.new
     #Most Nessus instances won't have a trusted cert.
@@ -31,7 +49,7 @@ class NessusautoRun
     @agent.idle_timeout = 1
     @agent.keep_alive=false
 
-    login_url = @host + '/login'
+    login_url = @host + 'login'
 
     login_response = @agent.post(login_url, {:login => username, :password => password})
 
@@ -47,7 +65,7 @@ class NessusautoRun
   end
 
   def list_policies
-    list_policy_url = @host + '/policy/list'
+    list_policy_url = @host + 'policy/list'
     list_policy_response = @agent.get(list_policy_url)
     policy_xml = Nokogiri::XML(list_policy_response.body)
     policy_names = policy_xml.xpath('//reply/contents/policies/policy/policyName').collect {|name| name.text}
@@ -59,7 +77,7 @@ class NessusautoRun
 
   def run_scan(hosts, pol_id, s_name)
     #Todo: Need validation for hostnames and policy IDs
-    scan_url = @host + '/scan/new'
+    scan_url = @host + 'scan/new'
     scan_response = @agent.post(scan_url, {:target => hosts, :policy_id => pol_id, :scan_name => s_name})
     scan_response_xml = Nokogiri::XML(scan_response.body)
     scan_id = scan_response_xml.xpath('//reply/contents/scan/uuid').text
@@ -69,7 +87,7 @@ class NessusautoRun
 
   def get_report(name)
     #ToDo:  Add a call to the report list to check the scan status before trying to download it
-    report_url = @host + '/file/report/download'
+    report_url = @host + 'file/report/download'
     report_uid = @executed_scans[name]
     report_response = @agent.post(report_url, {:report => report_uid})
     puts report_response.body.size.to_s + " is the size of the report"
