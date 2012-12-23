2228dc0ef54f416e5df7655d6bfac07bb296eab0
Rory McCune <rory.mccune@scotsts.com>
2012-12-23 12:50:58 +0000

    More updated for nessusautorun.rb

--- nessusautorun.rb
+++ nessusautorun.rb
@@ -15,58 +15,52 @@ class NessusautoRun
 
     require 'uri'
 
-
-    #ToDo: fix this up to check for the presence of a port and have it add it if needed
-    #At the moment it's a hack for forgetful people (e.g. me)
-    #unless host =~ /8834\/*$/
-    #  host = host + ':8834'
-    #end
-    
     host_url = URI.parse(host)
 
     unless host_url.class == URI::HTTPS
-      abort("Sure you got the hostname right?")
+      abort("Sure you got the hostname right?\n Syntax is https://<hostname>:<port>")
     end
 
     #Assumption here is that Nessus runs on 8834 in future this should be an option but at the moment we'll hardcode it
     unless host_url.port == 8834
-      host_url.set_port = 8834
+      host_url.port=8834
     end
 
     @host = host_url.to_s
-
+    @username = username
+    @password = password
     @agent = Mechanize.new
     #Most Nessus instances won't have a trusted cert.
     @agent.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
     #Troubleshooting stuff
-    #@agent.set_proxy 'localhost', 8080
-    #@agent.idle_timeout = 1
-    #@agent.keep_alive=false
+    @agent.set_proxy 'localhost', 8080
+    @agent.idle_timeout = 1
+    @agent.keep_alive=false
 
+    self.login
+
+    @executed_scans = Hash.new
+  end
+
+  def login
     login_url = @host + '/login'
-
-    login_response = @agent.post(login_url, {:login => username, :password => password})
-
+    login_response = @agent.post(login_url, {:login => @username, :password => @password})
     login_response_xml = Nokogiri::XML(login_response.body)
-
     @token = login_response_xml.xpath('//token').text
-
     if @token.length > 1
       puts "logged in ok with a token of " + @token
     end
-
-    @executed_scans = Hash.new
   end
 
   def list_policies
     list_policy_url = @host + '/policy/list'
     list_policy_response = @agent.get(list_policy_url)
     policy_xml = Nokogiri::XML(list_policy_response.body)
-    policy_names = policy_xml.xpath('//reply/contents/policies/policy/policyName').collect {|name| name.text}
-    policy_ids = policy_xml.xpath('//reply/contents/policies/policy/policyID').collect {|id| id.text}
-    policy_names.length.times do |line|
-      puts "Policy ID : #{policy_ids[line - 1]} - Policy Name : #{policy_names[line - 1]}"
+    @policy_names = policy_xml.xpath('//reply/contents/policies/policy/policyName').collect {|name| name.text}
+    @policy_ids = policy_xml.xpath('//reply/contents/policies/policy/policyID').collect {|id| id.text}
+    @policy_names.length.times do |line|
+      puts "Policy ID : #{@policy_ids[line - 1]} - Policy Name : #{@policy_names[line - 1]}"
     end
   end
 
