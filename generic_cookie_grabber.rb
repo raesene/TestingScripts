3f9f82ff8ce564631c674b849301283c848d267a
Rory McCune <raesene@gmail.com>
2013-04-25 13:10:00 +0100

    Added Generic Cookie Grabber

---
+++ generic_cookie_grabber.rb
@@ -0,0 +1,66 @@
+#!/usr/bin/env ruby
+
+#Code to get the session tokens after a two stage login
+
+
+
+require 'mechanize'
+
+output_file = File.open('tokens','a+')
+for i in 1..500
+  agent = Mechanize.new
+
+  #Set proxy to burp for troubleshooting
+  #agent.set_proxy 'localhost', 8080
+  #agent.idle_timeout = 1
+  #agent.keep_alive=false
+
+
+  #Turn off SSL verify when using burp
+  agent.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+
+  #get the pre-login page
+  page = agent.get('http://127.0.0.1:3000/login/start')
+
+  login_form = page.forms[0]
+
+  #Fill in your customer ID here change field name as necessary
+  login_form.field('username').value = 'admin'
+
+  page2 = login_form.click_button
+
+  login_form2 = page2.forms[0]
+
+  password = ['b','s','i','d','e','s']
+
+  c1 = page2.labels[0].to_s[/\d/]
+  c2 = page2.labels[1].to_s[/\d/]
+  c3 = page2.labels[2].to_s[/\d/]
+
+  c1 = c1.to_i - 1
+  c2 = c2.to_i - 1
+  c3 = c3.to_i - 1
+
+  
+  login_form2.field('char1').value = password[c1]
+  login_form2.field('char2').value = password[c2]
+  login_form2.field('char3').value = password[c3]
+
+  page3 = login_form2.click_button
+
+  if page3.body[/[a-z]{50}/]
+    output_file.puts page3.body[/[a-z]{50}/]
+  else
+    puts "didn't get a token"
+  end
+
+  agent.cookies.each do |cookie|
+    if cookie.name =~ /<cookie_We_want>/
+      output_file.puts cookie.value
+      puts 'captured a cookie'
+    end
+  end
+
+end
+
