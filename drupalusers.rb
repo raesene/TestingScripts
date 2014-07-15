555c52eeae5c6d053256dcc37bc26f536bc3846e
Rory McCune <raesene@gmail.com>
2014-07-15 15:52:05 +0100

    Added verbiage, started on error handling

--- drupalusers.rb
+++ drupalusers.rb
@@ -1,7 +1,37 @@
 #!/usr/bin/env ruby
+  # == Synopsis  
+  # This script iterates over the autocomplete view for a DRupal site and uses this to construct a list of user accounts
+  # Currently just does a-z so will miss ones that start differently, but a decent starting point.
+  # == Author
+  # Author::  Rory McCune
+  # Copyright:: Copyright (c) 2014 Rory Mccune
+  # License:: GPLv3
+  #
+  # This program is free software: you can redistribute it and/or modify
+  # it under the terms of the GNU General Public License as published by
+  # the Free Software Foundation, either version 3 of the License, or
+  # (at your option) any later version.
+  #
+  # This program is distributed in the hope that it will be useful,
+  # but WITHOUT ANY WARRANTY; without even the implied warranty of
+  # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  # GNU General Public License for more details.
+  #
+  # You should have received a copy of the GNU General Public License
+  # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  #
+  # == Usage 
+  #
+  #   drupalusers.rb <hostname>
 
-require 'httparty'
-require 'json'
+begin
+  require 'httparty'
+  require 'json'
+
+  	
+  rescue LoadError => e
+
+end  
 
 
 if ARGV.length < 1
